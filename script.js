a78695a4631a3f0ace7fa6711444b84d2540ec9e
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-06 12:16:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function isSameType(value1, value2) {
 	 if(typeof value1===typeof value2){
           return true;
       }
-		 else if(typeof value1===NaN) return true;
-		 else if(typeof value2===NaN) return true;
+		 // else if(typeof value1===NaN) return true;
+		 // else if(typeof value2===NaN) return true;
       else {
         return false;
       }
